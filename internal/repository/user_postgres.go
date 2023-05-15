5b6e793be2141009539d5910afdd3d6e133a4c46
sdf01fds <sdf0106@protonmail.com>
2023-05-15 19:46:16 +0500

    feature: choosing user role added

--- internal/repository/user_postgres.go
+++ internal/repository/user_postgres.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/sdf0106/ip-project/internal/domain"
 )
@@ -16,16 +19,29 @@ func NewUserPostgres(db *pgxpool.Pool) *UserPostgres {
 }
 
 func (r *UserPostgres) ChooseRole(userId int, role string) error {
+	var table string
 
-	return nil
+	switch role {
+	case "client":
+		table = clientsTable
+	case "owner":
+		table = ownersTable
+	case "agent":
+		table = agentsTable
+	default:
+		return errors.New("invalid type of user")
+	}
+
+	query := fmt.Sprintf("INSERT INTO %s (user_id) VALUES($1)", table)
+	_, err := r.db.Exec(context.Background(), query, userId)
+
+	return err
 }
 
 func (r *UserPostgres) UpdateUserInfo(userId int, user domain.User) (domain.User, error) {
-
 	return user, nil
 }
 
 func (r *UserPostgres) ChangeRole(userId int, role string) error {
-
 	return nil
 }
