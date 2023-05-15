db51c88449cfe97681cf686bb20f41592f6160d8
Sadikov Garik <sadikovgarik@gmail.com>
2023-05-15 21:04:30 +0500

    updateUser

--- internal/repository/user_postgres.go
+++ internal/repository/user_postgres.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/sdf0106/ip-project/internal/domain"
+	"github.com/sdf0106/ip-project/internal/dto"
+	"log"
+	"strings"
 )
 
 type UserPostgres struct {
@@ -38,10 +41,41 @@ func (r *UserPostgres) ChooseRole(userId int, role string) error {
 	return err
 }
 
-func (r *UserPostgres) UpdateUserInfo(userId int, user domain.User) (domain.User, error) {
-	return user, nil
+func (r *UserPostgres) UpdateUserInfo(userId int, user dto.UpdateUserInput) (domain.User, error) {
+	setValues := make([]string, 0)
+	args := make([]interface{}, 0)
+	argId := 1
+
+	if user.Name != "" {
+		setValues = append(setValues, fmt.Sprintf("name=$%d", argId))
+		args = append(args, user.Name)
+		argId++
+	}
+	if user.Email != "" {
+		setValues = append(setValues, fmt.Sprintf("email=$%d", argId))
+		args = append(args, user.Email)
+		argId++
+	}
+	setQuery := strings.Join(setValues, ", ")
+	query := fmt.Sprintf("UPDATE books SET %s WHERE id=$%d", setQuery, argId)
+	args = append(args, userId)
+
+	_, err := r.db.Exec(context.Background(), query, args...)
+	if err != nil {
+		log.Println(err)
+	}
+	var result domain.User
+	result.Name = user.Name
+	result.Email = user.Email
+	return result, nil
 }
 
 func (r *UserPostgres) ChangeRole(userId int, role string) error {
+	query := fmt.Sprintf("UPDATE %s Set user_type=%s WHERE user_id=$1", usersTable, role)
+
+	_, err := r.db.Exec(context.Background(), query, userId)
+	if err != nil {
+		log.Println(err)
+	}
 	return nil
 }
